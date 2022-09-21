a6d5adbae57a4902351a51ef1a35dd0911d651f2
KET <varlamova_e.t@mail.ru>
2022-09-21 19:04:44 +0300

    1t

---
+++ 1t/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] FillMatrix(int rows, int columns)
+{
+    double[,] matr = new double[rows, columns];
+    Random rnd = new Random();
+    for(int i = 0; i < matr.GetLength(0); i++)
+    {
+        for(int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.NextDouble()*100;
+            matr[i, j] = Math.Round(matr[i, j], 2);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 5}, ");
+            else Console.WriteLine($"{matrix[i, j], 5}");
+        }
+    }
+}
+
+double[,] table = FillMatrix(m, n);
+PrintMatrix(table);
