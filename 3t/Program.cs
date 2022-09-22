59d84138a029e894dce23474550e873d2adcfdd1
KET <varlamova_e.t@mail.ru>
2022-09-22 19:13:41 +0300

    3t

---
+++ 3t/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите количество строк матрицы: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+
+int[,] FillMatrix(int[,] matr, int min, int max)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j < matr.GetLength(1)-1) Console.Write($"{matr[i, j], 3} ");
+            else Console.WriteLine($"{matr[i, j], 3}");
+        }
+    }
+}
+
+void AverageColumnNumbers(int[,] matr)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matr.GetLength(0); i++)
+        {
+            sum += matr[i, j]; 
+        }
+        Console.WriteLine(sum/matr.GetLength(0));
+    }
+}
+
+int[,] resultmatrix = FillMatrix(matrix, 1, 10);
+PrintMatrix(resultmatrix);
+AverageColumnNumbers(resultmatrix);
